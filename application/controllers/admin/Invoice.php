4a606464db06641c3f800f2f0e7dea89713c4772
Fariz Yoga Syahputra <fariz.yoga@gmail.com>
2017-04-05 09:02:19 +0700

    finalizing transaction

--- application/controllers/admin/Invoice.php
+++ application/controllers/admin/Invoice.php
@@ -27,6 +27,22 @@ class Invoice extends CI_Controller
 
 	public function nonspk_save()
 	{
-		return $this->input->post();
+		$post = $this->input->post();
+
+		$params = [
+			'tgl_jual' => $post['tgl_jual'],
+			'no_jual' => $post['no_jual'],
+			'customer_id' => $post['customer_id'],
+			'spk' => $post['no_spk'],
+			'status' => 'ANTRIAN',
+			'total' => $post['total'],
+			'discount' => $post['discount'],
+			'charge' => $post['charge'],
+			'netto' => $post['netto'],
+			'bayar' => $post['bayar'],
+			'kembali' => $post['sisa']
+		];
+
+		$this->db->insert('tbl_header_jual', $params);
 	}
 }
